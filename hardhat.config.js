a7bccebb61d0c04bf24d616fd8d18f8964b6ff7a
JacenYi <jacenyicomeon@gmail.com>
2026-05-15 10:43:16 +0800

    Update hardhat.config.js
    V2  commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,10 @@ require("@nomicfoundation/hardhat-ethers");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.27",
 
   networks: {
-    zamaSepolia: {
+    sepolia: {
       url: "https://sepolia.infura.io/v3/[YOUR_INFURA_KEY]",
       accounts: [process.env.PRIVATE_KEY],
       chainId: 11155111,
